b9d9a4422f96356dbfa8161b1ee4fa4a707249ce
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-05-09 13:51:29 +0200

    Document and test errors

--- protocol/errors.go
+++ protocol/errors.go
@@ -5,8 +5,12 @@ import (
 	"io"
 )
 
+// strError is a simple string-based error type that implements the error interface.
+// It allows creating lightweight error values from string constants without
+// allocating a new error for each instance.
 type strError string
 
+// Error implements the error interface by returning the string value of the error.
 func (e strError) Error() string {
 	return string(e)
 }
