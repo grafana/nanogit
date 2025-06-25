c9fd926edfcbfe1826e9d32bfb91715ae0a19b65
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-06-25 07:37:43 +0200

    More tests

--- protocol/client/receivepack.go
+++ protocol/client/receivepack.go
@@ -53,7 +53,9 @@ func (c *rawClient) ReceivePack(ctx context.Context, data io.Reader) ([]byte, er
 	// Check for Git protocol errors in receive-pack response
 	if _, _, err := protocol.ParsePack(responseBody); err != nil {
 		logger.Debug("Protocol error detected in receive-pack response", "error", err.Error())
-		return responseBody, err
+		return nil, fmt.Errorf("parse pack: %w", err)
+	} else {
+		logger.Debug("Receive-pack response parsed successfully", "responseSize", len(responseBody), "error", err)
 	}
 
 	return responseBody, nil
